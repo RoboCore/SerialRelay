2488164bfb5714926588083cf3e33eb44c61568d
gabriel <gabriel@robocore.net>
2015-07-16 19:05:34 -0300

    v1.0 - 16/07/15
    Added Blink example.
    
    Signed-off-by: gabriel <gabriel@robocore.net>

---
+++ examples/Blink/Blink.ino
@@ -0,0 +1,36 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+/*
+      RoboCore - Serial Relay example
+                    (16/07/2015)
+
+  Written by François (based on default Blink example).
+  
+  Example of how to use the Serial Relay library.
+  The relay 1 of module 1 will turn on for one
+  second, then off for one second, repeatedly.
+
+*/
+
+#include <SerialRelay.h>
+
+SerialRelay relays(4,5,1); // (data, clock, number of modules)
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // nothing to do here
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  relays.SetRelay(1, SERIAL_RELAY_ON, 1);   // turn the relay ON
+  delay(1000);                              // wait for a second
+  relays.SetRelay(1, SERIAL_RELAY_OFF, 1);  // turn the relay OFF
+  delay(1000);                              // wait for a second
+}
+
+
